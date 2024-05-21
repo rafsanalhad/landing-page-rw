6064d4e2eff70a85d310cb6c03ac8d348c65be97
rendypuku <2241720124@polinema.ac.id>
2024-05-21 11:44:47 +0700

    p

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -8,8 +8,8 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <title>Hello, world!</title>
+    <link rel="stylesheet" href="{{asset ('css/style.css')}}">
+    <title>Sistem Informasi RW</title>
 </head>
 
 <body>
@@ -34,7 +34,7 @@
 
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        <a class="nav-link active" href="{{url ('/admin')}}" tabindex="-1" aria-active="true">Login</a>
                     </li>
                 </ul>
             </div>
@@ -48,7 +48,7 @@
                         Kec. Blimbing, Kota Malang</h3>
                 </div>
                 <div class="col-md-6 d-flex justify-content-center align-items-center">
-                    <img src="Elemen.png" class="img_hero" alt="">
+                    <img src="{{ asset ('images/Elemen.png')}}" class="img_hero" alt="">
                 </div>
             </div>
         </div>
@@ -87,7 +87,7 @@
             <div class="row">
                 <div class="col-md-4 d-flex align-items-center">
                     <div class="iconWrapper d-flex justify-content-center align-items-center">
-                        <img src="Vector.png" alt="" class="iconIngfo" srcset="">
+                        <img src="{{ asset ('images/Vector.png')}}" alt="" class="iconIngfo" srcset="">
                     </div>
                     <div class="contentIngfo ms-3">
                         <h3>4000+</h3>
@@ -96,7 +96,7 @@
                 </div>
                 <div class="col-md-4 d-flex align-items-center">
                     <div class="iconWrapper d-flex justify-content-center align-items-center">
-                        <img src="Vector.png" alt="" class="iconIngfo" srcset="">
+                        <img src="{{ asset ('images/Vector.png')}}" alt="" class="iconIngfo" srcset="">
                     </div>
                     <div class="contentIngfo ms-3">
                         <h3>4000+</h3>
@@ -105,7 +105,7 @@
                 </div>
                 <div class="col-md-4 d-flex align-items-center">
                     <div class="iconWrapper d-flex justify-content-center align-items-center">
-                        <img src="Vector.png" alt="" class="iconIngfo" srcset="">
+                        <img src="{{ asset ('images/Vector.png')}}" alt="" class="iconIngfo" srcset="">
                     </div>
                     <div class="contentIngfo ms-3">
                         <h3>4000+</h3>
@@ -119,7 +119,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-5 d-flex align-items-center">
-                    <img src="bg.png" class="about_img" alt="">
+                    <img src="{{ asset ('images/bg.png')}}" class="about_img" alt="">
                 </div>
                 <div class="col-md-7">
                     <h3 class="about_header">Berkenalan dengan RW03 Bunulrejo, Yuk!</h3>
@@ -136,7 +136,7 @@
             <div class="row">
                 <div class="col-md-4">
                     <div class="card card_program shadow-lg">
-                        <img src="Foto.png" class="card_img_program card-img-top" alt="">
+                        <img src="{{ asset ('images/Foto.png')}}" class="card_img_program card-img-top" alt="">
                         <div class="card-body">
                         <h3 class="card_header">Nama Program</h3>
                         <p class="card-text">Lorem ipsum dolor sit amet consectetur. Massa integer venenatis habitasse purus tellus pretium elementum scelerisque. Libero sit justo placerat</p>
@@ -145,7 +145,7 @@
                 </div>
                 <div class="col-md-4">
                     <div class="card card_program shadow-lg">
-                        <img src="Foto.png" class="card_img_program card-img-top" alt="">
+                        <img src="{{ asset ('images/Foto.png')}}" class="card_img_program card-img-top" alt="">
                         <div class="card-body">
                         <h3 class="card_header">Nama Program</h3>
                         <p class="card-text">Lorem ipsum dolor sit amet consectetur. Massa integer venenatis habitasse purus tellus pretium elementum scelerisque. Libero sit justo placerat</p>
@@ -154,7 +154,7 @@
                 </div>
                 <div class="col-md-4">
                     <div class="card card_program shadow-lg">
-                        <img src="Foto.png" class="card_img_program card-img-top" alt="">
+                        <img src="{{ asset ('images/Foto.png')}}" class="card_img_program card-img-top" alt="">
                         <div class="card-body">
                         <h3 class="card_header">Nama Program</h3>
                         <p class="card-text">Lorem ipsum dolor sit amet consectetur. Massa integer venenatis habitasse purus tellus pretium elementum scelerisque. Libero sit justo placerat</p>
@@ -178,7 +178,7 @@
                     <div class="card card_umkm shadow-lg">
                         <div class="card-body">
                             <h3 class="header_umkm text-center">Nama Umkm</h3>
-                            <img src="anime.png" alt="" class="img_umkm">
+                            <img src="{{ asset ('images/anime.png')}}" alt="" class="img_umkm">
                         </div>
                     </div>
                 </div>
@@ -186,7 +186,7 @@
                     <div class="card card_umkm shadow-lg">
                         <div class="card-body">
                             <h3 class="header_umkm text-center">Nama Umkm</h3>
-                            <img src="anime.png" alt="" class="img_umkm">
+                            <img src="{{ asset ('images/anime.png')}}" alt="" class="img_umkm">
                         </div>
                     </div>
                 </div>
@@ -194,7 +194,7 @@
                     <div class="card card_umkm shadow-lg">
                         <div class="card-body">
                             <h3 class="header_umkm text-center">Nama Umkm</h3>
-                            <img src="anime.png" alt="" class="img_umkm">
+                            <img src="{{ asset ('images/anime.png')}}" alt="" class="img_umkm">
                         </div>
                     </div>
                 </div>
@@ -205,34 +205,109 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
-                    <h3 class="headerAgenda">Agenda Terdekat</h3>
+                    <h3 class="headerAgenda text-center">Agenda Terdekat</h3>
                 </div>
-                    <div class="d-flex align-items-center justify-content-between">
-                        <img src="foto_agenda.png" class="foto_agenda" alt="">
-                            <div class="labelMain">
-                                
-                                <div class="labelWrapper d-flex justify-content-center align-items-center">
+                <div class="col-md-12 mb-3">
+                    <div class="card shadow-lg agendaSub">
+                        <div class="card-body d-flex align-items-center justify-content-between">
+                            <img src="{{ asset ('images/foto_agenda.png')}}" class="foto_agenda" alt="">
+                                <div class="labelMain">
+                                    <div class="labelWrapper d-flex justify-content-center align-items-center">
                                     Label
+                                    </div>
+                                    <h3 class="labelText">Nama Acara</h3>
                                 </div>
-                                <h3 class="labelText">Nama Acara</h3>
+                                <div class="tempatMain text-center">
+                                    <p class="agendaHeader">Tempat</p>
+                                    <h3 class="agendaText">Tempatnya</h3>
+                                </div>
+                                <div class="waktuMain text-center">
+                                    <p class="agendaHeader">Tempat</p>
+                                    <h3 class="agendaText">Tempatnya</h3>
+                                </div>
+                                <div class="penyelenggaraMain text-center">
+                                    <p class="agendaHeader">Tempat</p>
+                                    <h3 class="agendaText">Tempatnya</h3>
+                                </div>
                             </div>
-                            <div class="tempatMain text-center">
-                                <p class="agendaHeader">Tempat</p>
-                                <h3 class="agendaText">Tempatnya</h3>
-                            </div>
-                            <div class="waktuMain text-center">
-                                 <p class="agendaHeader">Tempat</p>
-                                <h3 class="agendaText">Tempatnya</h3>
-                            </div>
-                            <div class="penyelenggaraMain text-center">
-                                 <p class="agendaHeader">Tempat</p>
-                                <h3 class="agendaText">Tempatnya</h3>
-                            </div>
+                        </div>
                     </div>
-                    
+                </div>
+                <div class="col-md-12 mb-3">
+                    <div class="card shadow-lg agendaSub">
+                        <div class="card-body d-flex align-items-center justify-content-between">
+                            <img src="{{ asset ('images/foto_agenda.png')}}" class="foto_agenda" alt="">
+                                <div class="labelMain">
+                                    <div class="labelWrapper d-flex justify-content-center align-items-center">
+                                    Label
+                                    </div>
+                                    <h3 class="labelText">Nama Acara</h3>
+                                </div>
+                                <div class="tempatMain text-center">
+                                    <p class="agendaHeader">Tempat</p>
+                                    <h3 class="agendaText">Tempatnya</h3>
+                                </div>
+                                <div class="waktuMain text-center">
+                                    <p class="agendaHeader">Tempat</p>
+                                    <h3 class="agendaText">Tempatnya</h3>
+                                </div>
+                                <div class="penyelenggaraMain text-center">
+                                    <p class="agendaHeader">Tempat</p>
+                                    <h3 class="agendaText">Tempatnya</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12 mb-3">
+                    <div class="card shadow-lg agendaSub">
+                        <div class="card-body d-flex align-items-center justify-content-between">
+                            <img src="{{ asset ('images/foto_agenda.png')}}" class="foto_agenda" alt="">
+                                <div class="labelMain">
+                                    <div class="labelWrapper d-flex justify-content-center align-items-center">
+                                    Label
+                                    </div>
+                                    <h3 class="labelText">Nama Acara</h3>
+                                </div>
+                                <div class="tempatMain text-center">
+                                    <p class="agendaHeader">Tempat</p>
+                                    <h3 class="agendaText">Tempatnya</h3>
+                                </div>
+                                <div class="waktuMain text-center">
+                                    <p class="agendaHeader">Tempat</p>
+                                    <h3 class="agendaText">Tempatnya</h3>
+                                </div>
+                                <div class="penyelenggaraMain text-center">
+                                    <p class="agendaHeader">Tempat</p>
+                                    <h3 class="agendaText">Tempatnya</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+                                    
+                
+
             </div>
         </div>
     </section>
+    <section class="struktur">
+        <div class="container-fluid mb-5">
+            <div class="row">
+                <h3 class="headerStruktur text-center">Struktur Pengurus RW</h3>
+            </div>
+        </div>
+    </section>
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p>Hak Cipta &copy; 2024 Sistem Informasi RW - Kelompok 6</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
---
+++ resources/views/template/header.blade.php
@@ -0,0 +1,12 @@
+<nav class="flex items-center">
+    <ul class="flex-1 text-left">
+      <li class="list-none inline-block px-5"><a href="{{url('beranda')}}" class="underline text-white font-semibold px-2">RW 03 - Bunulrejo</a></li>
+    </ul>
+    <ul class="flex-1 text-right">
+      <li class="list-none inline-block px-5"><a href="{{url('beranda')}}" class="text-white px-2 hover:underline-offset-auto">Beranda</a></li>
+      <li class="list-none inline-block px-5"><a href="#" class=" text-white px-2 hover:underline">Tentang</a></li>
+      <li class="list-none inline-block px-5"><a href="#" class="text-white px-2 hover:underline-offset-8">UMKM</a></li>
+      <li class="list-none inline-block px-5"><a href="#" class="text-white px-2 hover:underline-offset-auto">Berita</a></li>
+      <li class="list-none inline-block px-5"><a href="#" class="text-white px-2 hover:underline-offset-auto">Kontak</a></li>
+    </ul>
+</nav>
